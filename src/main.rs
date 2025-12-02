07a4120da9a72b7843e11b45b6d7f49600886a64
Paco Algar Muñoz <paco.algar.15@gmail.com>
2025-12-02 01:43:23 +0100

    Add mini benchmark in main

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,37 @@
 mod backends;
 mod tensor;
 
+use crate::backends::Cuda;
+use std::time::Instant;
 use tensor::Tensor;
 
 pub type InfersResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 fn main() -> InfersResult<()> {
-    #[cfg(feature = "cuda")]
-    {
-        use crate::backends::Cuda;
-        println!("Running on CUDA");
-        let t1 = Tensor::rand(&[2, 2]).to::<Cuda>()?;
-        let t2 = Tensor::rand(&[2, 2]).to::<Cuda>()?;
-        println!("{t1}");
-        println!("{t2}");
-        let t3 = t1 + t2;
-        println!("{t3}");
-    }
+    let t1 = Tensor::rand(&[500, 500, 500]);
+    let t2 = Tensor::rand(&[500, 500, 500]);
+    let t1_gpu = t1.to::<Cuda>()?;
+    let t2_gpu = t2.to::<Cuda>()?;
+
+    println!("t1: {}", t1);
+    println!("t1: {}", t1);
 
     println!("Running on CPU");
-    let t1 = Tensor::rand(&[2, 2]);
-    let t2 = Tensor::rand(&[2, 2]);
-    println!("t1: {}", t1);
-    println!("t1: {}", t1);
-
+    let start = Instant::now();
     let t3 = t1 + t2;
-    println!("{t3}");
+    let duration = start.elapsed();
+    println!("Duration: {:?}", duration);
+    println!("{t3}\n");
+
+    #[cfg(feature = "cuda")]
+    {
+        println!("Running on CUDA");
+        let start = Instant::now();
+        let t3 = t1_gpu + t2_gpu;
+        let duration = start.elapsed();
+        println!("Duration: {:?}", duration);
+        println!("{t3}");
+    }
 
     Ok(())
 }
