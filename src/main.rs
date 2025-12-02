4d9d8e141e884bd5a1a54acaf03908af965e5231
Paco Algar Muñoz <paco.algar.15@gmail.com>
2025-12-02 02:22:12 +0100

    Improve main function

--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,25 @@ use tensor::Tensor;
 pub type InfersResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 fn main() -> InfersResult<()> {
-    let t1 = Tensor::rand(&[500, 500, 500]);
-    let t2 = Tensor::rand(&[500, 500, 500]);
+    let start = Instant::now();
+    let t1 = Tensor::rand(&[700, 700, 700]);
+    let t2 = Tensor::rand(&[700, 700, 700]);
+    println!("Duration: {:?}", start.elapsed());
     let t1_gpu = t1.to::<Cuda>()?;
     let t2_gpu = t2.to::<Cuda>()?;
 
-    println!("t1: {}", t1);
-    println!("t1: {}", t1);
-
     println!("Running on CPU");
     let start = Instant::now();
-    let t3 = t1 + t2;
-    let duration = start.elapsed();
-    println!("Duration: {:?}", duration);
-    println!("{t3}\n");
+    let _ = t1 + t2;
+    println!("Duration: {:?}", start.elapsed());
 
     #[cfg(feature = "cuda")]
     {
         println!("Running on CUDA");
         let start = Instant::now();
-        let t3 = t1_gpu + t2_gpu;
-        let duration = start.elapsed();
-        println!("Duration: {:?}", duration);
-        println!("{t3}");
+        let _ = t1_gpu + t2_gpu;
+        println!("Duration: {:?}", start.elapsed());
+        // println!("{t3}");
     }
 
     Ok(())
