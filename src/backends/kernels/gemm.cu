854ef35c6b3564b4871f03a22203dff501785e97
Paco Algar Muñoz <paco.algar.15@gmail.com>
2025-12-09 16:33:58 +0100

    Improve gemm and mul cuda kernels

--- src/backends/kernels/gemm.cu
+++ src/backends/kernels/gemm.cu
@@ -1,16 +1,40 @@
 extern "C" __global__ void gemm(int m, int n, int k, float alpha,
                                 const float *a, const float *b, float beta,
                                 float *c) {
-  int row = blockIdx.y * blockDim.y + threadIdx.y;
-  int col = blockIdx.x * blockDim.x + threadIdx.x;
-
-  if (row < m && col < n) {
-    float sum = 0.0f;
-
-    for (int p = 0; p < k; p++) {
-      sum += a[row * k + p] * b[p * n + col];
+  __shared__ float As[16][16];
+  __shared__ float Bs[16][16];
+  
+  int bx = blockIdx.x, by = blockIdx.y;
+  int tx = threadIdx.x, ty = threadIdx.y;
+  
+  int row = by * 16 + ty;
+  int col = bx * 16 + tx;
+  
+  float sum = 0.0f;
+  
+  for (int p = 0; p < (k + 15) / 16; ++p) {
+    if (row < m && p * 16 + tx < k) {
+      As[ty][tx] = a[row * k + p * 16 + tx];
+    } else {
+      As[ty][tx] = 0.0f;
     }
-
+    
+    if (p * 16 + ty < k && col < n) {
+      Bs[ty][tx] = b[(p * 16 + ty) * n + col];
+    } else {
+      Bs[ty][tx] = 0.0f;
+    }
+    
+    __syncthreads();
+    
+    for (int i = 0; i < 16; ++i) {
+      sum += As[ty][i] * Bs[i][tx];
+    }
+    
+    __syncthreads();
+  }
+  
+  if (row < m && col < n) {
     float cOld = c[row * n + col];
     c[row * n + col] = alpha * sum + beta * cOld;
   }
--- src/backends/kernels/mul.cu
+++ src/backends/kernels/mul.cu
@@ -1,4 +1,4 @@
-extern "C" __global__ void add(const float *a, const float *b, float *c,
+extern "C" __global__ void mul(const float *a, const float *b, float *c,
                                size_t n) {
   size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
   if (idx < n) {
