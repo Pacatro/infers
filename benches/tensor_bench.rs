849dc5b50eec176f5e08226c98127ce2127d4609
Paco Algar Muñoz <paco.algar.15@gmail.com>
2025-12-10 21:48:18 +0100

    Add new bench

--- benches/tensor_bench.rs
+++ benches/tensor_bench.rs
@@ -3,12 +3,13 @@ use std::hint::black_box;
 
 use infers::Tensor;
 
-const BENCH_GROUP: &str = "Matrix Multiplication (N x N)";
+const BENCH_MATMUL_GROUP: &str = "Matrix Multiplication (N x N)";
+const BENCH_TRANSPOSE_GROUP: &str = "Transpose";
 const SIZES: &[usize] = &[64, 128, 256, 512, 1024, 2048];
 const SAMPLE_SIZE: usize = 10;
 
-fn bench_cpu_sizes(c: &mut Criterion) {
-    let mut group = c.benchmark_group(BENCH_GROUP);
+fn bench_matmul_cpu(c: &mut Criterion) {
+    let mut group = c.benchmark_group(BENCH_MATMUL_GROUP);
 
     for &n in SIZES {
         let bench_id = format!("CPU_{}x{}", n, n);
@@ -27,9 +28,9 @@ fn bench_cpu_sizes(c: &mut Criterion) {
 }
 
 #[cfg(feature = "cuda")]
-fn bench_cuda_sizes(c: &mut Criterion) {
+fn bench_matmul_cuda(c: &mut Criterion) {
     use infers::backends::Cuda;
-    let mut group = c.benchmark_group(BENCH_GROUP);
+    let mut group = c.benchmark_group(BENCH_MATMUL_GROUP);
 
     for &n in SIZES {
         let bench_id = format!("CUDA_{}x{}", n, n);
@@ -47,15 +48,40 @@ fn bench_cuda_sizes(c: &mut Criterion) {
     group.finish();
 }
 
-fn bench_all(c: &mut Criterion) {
-    bench_cpu_sizes(c);
+fn bench_matmul(c: &mut Criterion) {
+    bench_matmul_cpu(c);
     #[cfg(feature = "cuda")]
-    bench_cuda_sizes(c);
+    bench_matmul_cuda(c);
+}
+
+fn bench_transpose(c: &mut Criterion) {
+    let mut group = c.benchmark_group(BENCH_TRANSPOSE_GROUP);
+
+    for (&n, &m) in SIZES.iter().zip(SIZES.iter().rev()) {
+        let bench_id = format!("Transpose_{}x{}", n, m);
+
+        let t = Tensor::rand(&[n, m]);
+
+        group.bench_function(&bench_id, |b| {
+            b.iter(|| {
+                let r = t.t();
+                black_box(r);
+            });
+        });
+    }
+    group.finish();
 }
 
 criterion_group! {
-    name = benches;
+    name = matmul_benches;
     config = Criterion::default().sample_size(SAMPLE_SIZE);
-    targets = bench_all
+    targets = bench_matmul
 }
-criterion_main!(benches);
+
+criterion_group! {
+    name = transpose_benches;
+    config = Criterion::default().sample_size(SAMPLE_SIZE);
+    targets = bench_transpose
+}
+
+criterion_main!(transpose_benches);
